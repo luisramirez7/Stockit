a9a1027938ef32466edc87e2db7a5a7cea5718e1
Tanner <tannn@users.noreply.github.com>
2017-09-16 19:33:36 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def mergeCsvOnSymbols(file1, file2):
+	df1 = pd.read_csv(file1)
+	df2 = pd.read_csv(file2)
+	result = pd.merge(df1, df2, on="symbol", how="inner")
+	return result
