edea14cd42790d5b826a2771333491a69930222c
luisramirez7 <luisramirez2@cox.net>
2017-09-17 01:18:34 -0400

    final data clean and death and dying

--- glue-code.py
+++ glue-code.py
@@ -1,6 +1,12 @@
+from mergeCsvOnSymbol import *
+from date_to_day_and_month import *
+from calculateChangeInPrice import *
 import os
 import tarfile
+
 tonsofdata = "tonsofdata"
+superempty = "/Users/luis/Documents/hackingstuff/hack-stock/superempty"
+
 def unTarMe(fileDirectory):
 	for filename in os.listdir('/Users/luis/Documents/hackingstuff/hack-stock/tonsofdata'):
 		extension = os.path.splitext(filename)[1]
@@ -10,4 +16,18 @@ def unTarMe(fileDirectory):
 			tar.extractall(path="/Users/luis/Documents/hackingstuff/hack-stock/tonsofdata/" + prefix)
 			tar.close()
 
-unTarMe(tonsofdata)
+
+def final_data(folderdirectory):
+	industries_filepath = "/Users/luis/Documents/hackingstuff/hack-stock/industries.csv"
+	for foldername in os.listdir(folderdirectory):
+		if foldername==".DS_Store" or foldername=="tonsofdata20161010.tar":
+			continue
+		prices_filepath = "/Users/luis/Documents/hackingstuff/hack-stock/superempty/" + foldername + "/prices.csv"
+		output = mergeCsvOnSymbol(file1=industries_filepath, file2=prices_filepath)
+		dateInputForLooE = output["date"]
+		date_add = isodateConverter(dateInputForLooE, 0)
+		output["weekday"] = date_add
+		final_data_clean = changeInPrice(output)
+		print(final_data_clean)
+
+final_data(superempty)
